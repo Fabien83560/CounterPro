1c23679a6b8deb7333a779e47d9a6831796bb24c
Fabien83560 <fabienortega.290604@gmail.com>
2024-08-11 19:12:49 +0200

    Add config-example.js

---
+++ config-example.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "MEHGFYZTEHBJUZ545679ZZ9543ZFGHZJOEGFZ5EZ435ERFZYGHIEJZH5E67Z65ERFGZHIJ",
+    applicationClientId: "8732453627802673",
+    databaseHost: '79.99.30.12',
+    databaseUser: 'myUser',
+    databasePassword: 'myBigPassword',
+    databaseName: 'databaseName',
+    databasePort: 3306
+}
