12adabd995824d4ad61c37a0d7fafbf653802bd8
Fabien83560 <fabienortega.290604@gmail.com>
2024-08-23 21:37:22 +0200

    Handle error in guildUpdate.js

--- application/Events/guildUpdate.js
+++ application/Events/guildUpdate.js
@@ -1,14 +1,15 @@
 const { selectDiscordServers, updateDiscordServerInfos } = require("../Functions/sql");
 
 module.exports = async (oldGuild, newGuild) => {
-    if (oldGuild.name !== newGuild.name) {
+    if (oldGuild && oldGuild.name && newGuild.name && oldGuild.name !== newGuild.name) {
         try {
             const server = await selectDiscordServers(newGuild.id);
             if (server.length > 0) {
                 await updateDiscordServerInfos(newGuild.id, newGuild.name);
+                console.log(`Mise à jour du nom de la guilde (${oldGuild.name}) dans la base de données : ${newGuild.name}`);
             }
         } catch (error) {
-            console.error('Error fetching or updating server info:', error.message);
+            console.error('Erreur lors de la récupération ou de la mise à jour des informations du serveur :', error.message);
         }
     }
 };
